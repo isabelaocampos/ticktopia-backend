59ffdc538f0e31a913c6e71ce3d7b555654a2228
elkofix <holasoygermamwow@gmail.com>
2025-05-07 15:43:16 -0500

    fix: test

--- src/event/event.service.spec.ts
+++ src/event/event.service.spec.ts
@@ -1,16 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EventService } from './event.service';
 
+const mockEventRepo = {};
+const mockUserRepo = {};
+
 describe('EventService', () => {
   let service: EventService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EventService],
+      providers: [
+        EventService,
+        { provide: 'EventRepository', useValue: mockEventRepo },
+        { provide: 'UserRepository', useValue: mockUserRepo },
+      ],
     }).compile();
 
     service = module.get<EventService>(EventService);
   });
+
 
   it('should be defined', () => {
     expect(service).toBeDefined();
--- src/ticket/ticket.controller.spec.ts
+++ src/ticket/ticket.controller.spec.ts
@@ -1,18 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TicketController } from './ticket.controller';
 import { TicketService } from './ticket.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Ticket } from './entities/ticket.entity';
+import { User } from '../auth/entities/user.entity';
+import { Presentation } from '../presentation/entities/presentation.entity';
 
 describe('TicketController', () => {
   let controller: TicketController;
 
+  const mockTicketService = {
+    findAll: jest.fn(() => ['ticket1', 'ticket2']), // ejemplo de método mock
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TicketController],
-      providers: [TicketService],
+      providers: [
+        {
+          provide: TicketService,
+          useValue: mockTicketService,
+        },
+      ],
     }).compile();
 
     controller = module.get<TicketController>(TicketController);
   });
+
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
