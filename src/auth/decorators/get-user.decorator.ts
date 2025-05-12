9588d325e1fbd37c97501fbd5d6206dd731274bd
elkofix <holasoygermamwow@gmail.com>
2025-05-12 09:24:50 -0500

    feat: adding coverage test

--- src/auth/decorators/get-user.decorator.ts
+++ src/auth/decorators/get-user.decorator.ts
@@ -1,12 +1,16 @@
+// get-user.decorator.ts
 import { createParamDecorator, ExecutionContext, InternalServerErrorException } from "@nestjs/common";
 
-export const GetUser = createParamDecorator(
-    (data, context : ExecutionContext) => {
-        const req = context.switchToHttp().getRequest();
-        
-        const user = req.user;
-        if(!user) throw new InternalServerErrorException(`User ${user.email} not found`);
 
-        return (!data) ? user: user[data];
-    }
-)
+export const getUserDecoratorFactory = (data: string | undefined, context: ExecutionContext): any => {
+  const req = context.switchToHttp().getRequest<{ user?: any| null }>(); 
+  const user = req.user;
+
+  if (!user) {
+    throw new InternalServerErrorException(`User not found`);
+  }
+
+  return!data? user : user[data];
+};
+
+export const GetUser = createParamDecorator(getUserDecoratorFactory);
